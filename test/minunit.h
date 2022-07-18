6f1b820914403f12fd43ed78b45c8240c72b7c43
zhoujian <zhoujian.ww@gmail.com>
2022-07-18 16:11:27 +0800

    test   使用log输出

--- test/minunit.h
+++ test/minunit.h
@@ -6,50 +6,47 @@ extern "C"
 {
 #endif
 
-#include "stdio.h"
+#define LOG_MODULE "minunit"
+#include "log.h"
 
 /* file: minunit.h */
-#define MU_ASSERT(test)               \
-    do                                \
-    {                                 \
-        if (!(test))                  \
-            printf("TEST: "           \
-                   "(" __FILE__ ":%d" \
-                   ":" #test ")\r\n", \
-                   __LINE__);         \
+#define MU_ASSERT(test)              \
+    do                               \
+    {                                \
+        if (!(test))                 \
+            LOG_W("(" __FILE__ ":%d" \
+                  ":" #test ")\r\n", \
+                  __LINE__);         \
     } while (0)
 
-#define MU_RUN(test)                  \
-    do                                \
-    {                                 \
-        if (!test())                  \
-            printf("TEST: "           \
-                   "(" __FILE__ ":%d" \
-                   ":" #test ")\r\n", \
-                   __LINE__);         \
+#define MU_RUN(test)                 \
+    do                               \
+    {                                \
+        if (!test())                 \
+            LOG_W("(" __FILE__ ":%d" \
+                  ":" #test ")\r\n", \
+                  __LINE__);         \
     } while (0)
 
-#define MU_ASSERT_EQUALS(a, b)             \
-    do                                     \
-    {                                      \
-        if (a != b)                        \
-            printf("TEST: "                \
-                   "(" __FILE__ ":%d"      \
-                   ":" #a "==" #b ")\r\n", \
-                   __LINE__);              \
+#define MU_ASSERT_EQUALS(a, b)            \
+    do                                    \
+    {                                     \
+        if (a != b)                       \
+            LOG_W("(" __FILE__ ":%d"      \
+                  ":" #a "==" #b ")\r\n", \
+                  __LINE__);              \
     } while (0)
 
-#define MU_ASSERT_VEC_EQUALS(a, b, size)               \
-    do                                                 \
-    {                                                  \
-        for (uint32_t i = 0; i < size; i++)            \
-        {                                              \
-            if (a[i] != b[i])                          \
-                printf("TEST: "                        \
-                       "(" __FILE__ ":%d"              \
-                       ":" #a "[%d]==" #b "[%d])\r\n", \
-                       __LINE__, i, i);                \
-        }                                              \
+#define MU_ASSERT_VEC_EQUALS(a, b, size)                \
+    do                                                  \
+    {                                                   \
+        for (uint32_t i = 0; i < size; i++)             \
+        {                                               \
+            if (a[i] != b[i])                           \
+                LOG_W("(" __FILE__ ":%d"                \
+                      ":" #a "[%lu]==" #b "[%lu])\r\n", \
+                      __LINE__, i, i);                  \
+        }                                               \
     } while (0)
 
 #define MU_VEC_CLEAR(vec, size)             \
