2b2f439160e2192d17c200c3d7209c92216ea35e
zhoujian <zhoujian.ww@gmail.com>
2022-08-28 21:11:25 +0800

    pwm 修复period计算误差

--- peripheral/stm32f1xx/pwm.cpp
+++ peripheral/stm32f1xx/pwm.cpp
@@ -34,27 +34,27 @@ Result Pwm::stop()
 
 Result Pwm::duty_set(PwmChannel channels, uint16_t duty)
 {
-    uint32_t ar = LL_TIM_GetAutoReload(_handle.Instance);
+    uint32_t period = LL_TIM_GetAutoReload(_handle.Instance) + 1;
 
     if (channels & (TimerChannel_1 | TimerChannel_1P | TimerChannel_1N))
     {
         LL_TIM_OC_SetCompareCH1(_handle.Instance,
-                                ar * duty / _config.fullScaleDuty);
+                                period * duty / _config.fullScaleDuty);
     }
     if (channels & (TimerChannel_2 | TimerChannel_2P | TimerChannel_2N))
     {
         LL_TIM_OC_SetCompareCH2(_handle.Instance,
-                                ar * duty / _config.fullScaleDuty);
+                                period * duty / _config.fullScaleDuty);
     }
     if (channels & (TimerChannel_3 | TimerChannel_3P | TimerChannel_3N))
     {
         LL_TIM_OC_SetCompareCH3(_handle.Instance,
-                                ar * duty / _config.fullScaleDuty);
+                                period * duty / _config.fullScaleDuty);
     }
     if (channels & (TimerChannel_4))
     {
         LL_TIM_OC_SetCompareCH4(_handle.Instance,
-                                ar * duty / _config.fullScaleDuty);
+                                period * duty / _config.fullScaleDuty);
     }
 
     return Result_OK;
