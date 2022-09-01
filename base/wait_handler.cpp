6f44ccbfbfdfef29321812d023a420c1170abb00
zhoujian <zhoujian.ww@gmail.com>
2022-09-01 15:54:49 +0800

    base   将三种waitHandler合并成一种, 并提供了分层机制.

--- base/wait_handler.cpp
+++ base/wait_handler.cpp
@@ -7,6 +7,12 @@ namespace ww
 
 using namespace ww::os;
 
+WaitHandler::WaitHandler(EventGroup &eventGroup, uint32_t doneFlag, uint32_t errorFlag)
+    : _eventGroup(eventGroup), _doneFlag(doneFlag), _errorFlag(errorFlag)
+{
+    initErrorCode = _eventGroup.initErrorCode;
+};
+
 void WaitHandler::set_value(void *value)
 {
     _value = value;
@@ -20,120 +26,70 @@ void *WaitHandler::get_sender()
     return _sender;
 };
 
-Result WaitHandler::wait()
+void WaitHandler::done_callback_set(void *receiver, Callback onDone)
 {
-    return wait(TIMEOUT_FOREVER);
-};
-
-void CallbackWaitHandler::done_callback_set(Callback onDone)
-{
+    _receiver = receiver;
     _onDone = onDone;
 };
-void CallbackWaitHandler::error_callback_set(Callback onError)
+void WaitHandler::error_callback_set(void *receiver, Callback onError)
 {
+    _receiver = receiver;
     _onError = onError;
 };
-Result CallbackWaitHandler::reset()
+
+// Result WaitHandler::wait()
+// {
+//     return wait(0, TIMEOUT_FOREVER);
+// };
+// Result WaitHandler::wait(uint32_t level)
+// {
+//     return wait(level, TIMEOUT_FOREVER);
+// };
+Result WaitHandler::wait(uint32_t level, uint32_t timeout)
 {
-    if (is_busy())
+    uint32_t events;
+    uint32_t startTick = Utils::tick_get();
+    uint32_t duration = 0;
+    for (; timeout > duration; duration = Utils::tick_diff(startTick))
     {
-        return Result_Busy;
-    }
-    _status.value = 0;
-    return Result_OK;
-};
-bool CallbackWaitHandler::is_busy()
-{
-    return !_status.is_done && !_status.is_error;
-};
-Result CallbackWaitHandler::wait(uint32_t timeout)
-{
-    if (timeout == TIMEOUT_NOWAIT)
-    {
-        if (_status.is_done)
+        auto rst =
+            _eventGroup.wait(_doneFlag | _errorFlag, events,
+                             EventOptions_WaitForAny | EventOptions_NoClear, timeout - duration);
+        if (rst == Result::Result_OK)
         {
-            return Result_OK;
-        }
-        else if (_status.is_error)
-        {
-            return Result_GeneralError;
+            if (events & _errorFlag)
+            {
+                return Result_GeneralError;
+            }
+            else if (events & _doneFlag)
+            {
+                if (level == _level)
+                {
+                    return Result_OK;
+                }
+                else
+                {
+                    _eventGroup.reset(_doneFlag);
+                }
+            }
+            else
+            {
+                // TODO: no possible to reach here
+                return Result_StatusReserved;
+            }
         }
         else
         {
             return Result_Timeout;
         }
     }
-    else
-    {
-        uint32_t start = Utils::tick_get();
-        while (!_status.is_done && !_status.is_error)
-        {
-            if (Utils::tick_diff(start) > timeout)
-            {
-                return Result_Timeout;
-            }
-        }
-        if (_status.is_done)
-        {
-            return Result_OK;
-        }
-        else if (_status.is_error)
-        {
-            return Result_GeneralError;
-        }
-        else
-        {
-            return Result_StatusReserved;
-        }
-    }
-};
-void CallbackWaitHandler::done_set(void *sender)
-{
-    _sender = sender;
-    _status.is_done = true;
-    if (_onDone)
-    {
-        _onDone(_sender, _value, _receiver);
-    }
-};
-void CallbackWaitHandler::error_set(void *sender)
-{
-    _sender = sender;
-    _status.is_error = true;
-    if (_onError)
-    {
-        _onError(_sender, _value, _receiver);
-    }
 };
 
-
-using namespace ww::os;
-
-EventGroupWaitHandler::EventGroupWaitHandler(EventGroup &eventGroup,
-                                             uint32_t doneFlag,
-                                             uint32_t errorFlag)
-    : _eventGroup(eventGroup), _doneFlag(doneFlag), _errorFlag(errorFlag)
-{
-
-    initErrorCode = _eventGroup.initErrorCode;
-};
-EventGroupWaitHandler::~EventGroupWaitHandler(){};
-
-Result EventGroupWaitHandler::reset()
-{
-    if (is_busy())
-    {
-        return Result_Busy;
-    }
-    _eventGroup.reset(_doneFlag | _errorFlag);
-    return Result_OK;
-};
-bool EventGroupWaitHandler::is_busy()
+bool WaitHandler::is_busy()
 {
     uint32_t events;
-    auto rst =
-        _eventGroup.wait(_doneFlag | _errorFlag, events,
-                         EventOptions_WaitForAny | EventOptions_NoClear, 0);
+    auto rst = _eventGroup.wait(_doneFlag | _errorFlag, events,
+                                EventOptions_WaitForAny | EventOptions_NoClear, 0);
     if (rst == Result::Result_OK)
     {
         return false;
@@ -144,43 +100,43 @@ bool EventGroupWaitHandler::is_busy()
     }
 };
 
-void EventGroupWaitHandler::done_set(void *sender)
+Result WaitHandler::reset()
+{
+    if (is_busy())
+    {
+        return Result_Busy;
+    }
+    _eventGroup.reset(_doneFlag | _errorFlag);
+    return Result_OK;
+};
+
+void WaitHandler::done_set(void *sender)
 {
     _sender = sender;
+    if (_onDone)
+    {
+        _onDone(_sender, _value, _receiver);
+    }
     _eventGroup.set(_doneFlag);
 };
-void EventGroupWaitHandler::error_set(void *sender)
+
+void WaitHandler::error_set(void *sender)
 {
     _sender = sender;
+    if (_onError)
+    {
+        _onError(_sender, _value, _receiver);
+    }
     _eventGroup.set(_errorFlag);
 };
-Result EventGroupWaitHandler::wait(uint32_t timeout)
+
+uint32_t WaitHandler::scope_begin()
 {
-
-    uint32_t events;
-
-    auto rst = _eventGroup.wait(_doneFlag | _errorFlag, events,
-                                EventOptions_WaitForAny | EventOptions_NoClear,
-                                timeout);
-    if (rst == Result::Result_OK)
-    {
-        if (events & _doneFlag)
-        {
-            return Result_OK;
-        }
-        else if (events & _errorFlag)
-        {
-            return Result_GeneralError;
-        }
-        else
-        {
-            // TODO: no possible to reach here
-            return Result_Timeout;
-        }
-    }
-    else
-    {
-        return Result_Timeout;
-    }
+    return ++_level;
 };
+void WaitHandler::scope_end()
+{
+    --_level;
+};
+
 } // namespace ww
--- base/wait_handler.hpp
+++ base/wait_handler.hpp
@@ -6,6 +6,9 @@
 namespace ww
 {
 
+using namespace ww::os;
+
+typedef void (*Callback)(void *sender, void *event, void *receiver);
 /**
  * @brief Implement the sender receiver async pattern.
  * Sender maintain the handler's state, then receiver wait for sender's done or
@@ -19,85 +22,33 @@ namespace ww
 class WaitHandler : public Initializable
 {
   public:
+    WaitHandler(EventGroup &eventGroup, uint32_t doneFlag, uint32_t errorFlag);
     void set_value(void *value);
     void *get_value();
     void *get_sender();
 
-    virtual Result reset() = 0;
-    virtual bool is_busy() = 0;
-    Result wait();
-    virtual Result wait(uint32_t timeout) = 0;
-    virtual void done_set(void *sender) = 0;
-    virtual void error_set(void *sender) = 0;
+    Result reset();
+    bool is_busy();
+    // Result wait();
+    // Result wait(uint32_t level);
+    Result wait(uint32_t level, uint32_t timeout);
+    void done_set(void *sender);
+    void error_set(void *sender);
+    void done_callback_set(void *receiver, Callback onDone);
+    void error_callback_set(void *receiver, Callback onError);
+    uint32_t scope_begin();
+    void scope_end();
 
   protected:
     void *_sender;
-
     void *_value;
-};
-
-typedef void (*Callback)(void *sender, void *event, void *receiver);
-
-/**
- * @brief
- *
- */
-class CallbackWaitHandler : public WaitHandler
-{
-  public:
-    CallbackWaitHandler(void *receiver) : _receiver(receiver)
-    {
-        initErrorCode = Result_OK;
-    };
-
-    CallbackWaitHandler(void *receiver, Callback onDone, Callback onError)
-        : _receiver(receiver), _onDone(onDone), _onError(onError)
-    {
-        initErrorCode = Result_OK;
-    };
-
-    void done_callback_set(Callback onDone);
-    void error_callback_set(Callback onError);
-    virtual Result reset();
-    virtual bool is_busy();
-
-    virtual Result wait(uint32_t timeout);
-
-    virtual void done_set(void *sender);
-    virtual void error_set(void *sender);
-
-  private:
-    void *_receiver;
-    Callback _onDone;
-    Callback _onError;
-    union _status {
-        struct
-        {
-            bool is_done : 1;
-            bool is_error : 1;
-        };
-        uint32_t value;
-    } _status;
-};
-
-using namespace ww::os;
-
-class EventGroupWaitHandler : public WaitHandler
-{
-  public:
-    EventGroupWaitHandler(EventGroup &eventGroup, uint32_t doneFlag, uint32_t errorFlag);
-
-    ~EventGroupWaitHandler();
-    virtual Result reset();
-    virtual bool is_busy();
-    virtual Result wait(uint32_t timeout);
-    virtual void done_set(void *sender);
-    virtual void error_set(void *sender);
-
-  private:
     EventGroup &_eventGroup;
     uint32_t _doneFlag;
     uint32_t _errorFlag;
+    void *_receiver;
+    Callback _onDone;
+    Callback _onError;
+    uint32_t _level;
 };
 
 } // namespace ww
