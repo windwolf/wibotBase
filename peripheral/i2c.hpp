417cd84475209d3160a5e70b46723d04be53d26c
zhoujian <zhoujian.ww@gmail.com>
2022-12-05 09:55:27 +0800

    i2c config增加注释

--- peripheral/i2c.hpp
+++ peripheral/i2c.hpp
@@ -7,53 +7,58 @@
 namespace wibot::peripheral
 {
 
-I2C_PER_DECL
+	I2C_PER_DECL
 
-union I2cMasterConfig {
-    struct
-    {
-        uint8_t txDmaThreshold : 8;
-        uint8_t rxDmaThreshold : 8;
-        uint8_t slaveAddress : 8;
-        DataWidth dataWidth : 2;
-        bool useTxDma : 1;
-        bool useRxDma : 1;
-        uint32_t : 4;
-    };
-    uint32_t value;
-};
+	union I2cMasterConfig
+	{
+		struct
+		{
+			uint8_t txDmaThreshold: 8;
+			uint8_t rxDmaThreshold: 8;
+			/**
+			 * @brief 7bit, must be shifted left 1 bit
+			 */
+			uint8_t slaveAddress: 8;
+			DataWidth dataWidth: 2;
+			bool useTxDma: 1;
+			bool useRxDma: 1;
+			uint32_t : 4;
+		};
+		uint32_t value;
+	};
 
-class I2cMaster : public Initializable
-{
-  public:
-    I2cMaster(I2C_CTOR_ARG);
-    ~I2cMaster();
-    Result _init() override;
-    void _deinit() override;
-    I2cMasterConfig &config_get();
-    Result read(uint32_t address, void *data, uint32_t size, WaitHandler &waitHandler);
-    Result write(uint32_t address, void *data, uint32_t size, WaitHandler &waitHandler);
-    Result read(void *data, uint32_t size, WaitHandler &waitHandler);
-    Result write(void *data, uint32_t size, WaitHandler &waitHandler);
+	class I2cMaster : public Initializable
+	{
+	 public:
+		I2cMaster(I2C_CTOR_ARG);
+		~I2cMaster();
+		Result _init() override;
+		void _deinit() override;
+		I2cMasterConfig& config_get();
+		Result read(uint32_t address, void* data, uint32_t size, WaitHandler& waitHandler);
+		Result write(uint32_t address, void* data, uint32_t size, WaitHandler& waitHandler);
+		Result read(void* data, uint32_t size, WaitHandler& waitHandler);
+		Result write(void* data, uint32_t size, WaitHandler& waitHandler);
 
-  private:
-    I2C_FIELD_DECL
-    I2cMasterConfig _config;
-    union {
-        struct
-        {
-            bool isReadDmaEnabled : 1;
-            bool isWriteDmaEnabled : 1;
-        };
-        uint32_t value;
-    } _status;
-    WaitHandler *_waitHandler;
-    Buffer8 _txBuffer;
-    Buffer8 _rxBuffer;
-    static void _on_read_complete_callback(I2C_CALLBACK_ARG);
-    static void _on_write_complete_callback(I2C_CALLBACK_ARG);
-    static void _on_error_callback(I2C_CALLBACK_ARG);
-};
+	 private:
+		I2C_FIELD_DECL
+		I2cMasterConfig _config;
+		union
+		{
+			struct
+			{
+				bool isReadDmaEnabled: 1;
+				bool isWriteDmaEnabled: 1;
+			};
+			uint32_t value;
+		} _status;
+		WaitHandler* _waitHandler;
+		Buffer8 _txBuffer;
+		Buffer8 _rxBuffer;
+		static void _on_read_complete_callback(I2C_CALLBACK_ARG);
+		static void _on_write_complete_callback(I2C_CALLBACK_ARG);
+		static void _on_error_callback(I2C_CALLBACK_ARG);
+	};
 
 }; // namespace wibot::peripheral
 
