41be9be902f28426d12079b284a3e976144b0ef1
zhoujian <zhoujian.ww@gmail.com>
2022-12-05 09:56:40 +0800

    修复 EventGroup的 TX port的错误

--- os/os.hpp
+++ os/os.hpp
@@ -7,52 +7,52 @@
 namespace wibot::os
 {
 
-class Utils
-{
-  public:
-    static void delay(uint32_t ms);
-    static uint32_t tick_get();
-    static uint32_t tick_diff(uint32_t tick);
-};
+	class Utils
+	{
+	 public:
+		static void delay(uint32_t ms);
+		static uint32_t tick_get();
+		static uint32_t tick_diff(uint32_t tick);
+	};
 
-class Thread
-{
-  public:
-    static void sleep(uint32_t ms);
-};
+	class Thread
+	{
+	 public:
+		static void sleep(uint32_t ms);
+	};
 
-class Mutex
-{
-  public:
-    Mutex(const char *name);
-    ~Mutex();
-    bool lock(uint32_t timeout);
-    void unlock();
+	class Mutex
+	{
+	 public:
+		Mutex(const char* name);
+		~Mutex();
+		bool lock(uint32_t timeout);
+		void unlock();
 
-  private:
-    MUTEX_TYPEDEF _instance;
-};
+	 private:
+		MUTEX_TYPEDEF _instance;
+	};
 
-typedef uint8_t EventOptions;
-#define EventOptions_Wait_Flag 0x01
+	typedef uint8_t EventOptions;
+#define EventOptions_Wait_Flag 0x02
 #define EventOptions_WaitForAny 0x00
 #define EventOptions_WaitForAll EventOptions_Wait_Flag
-#define EventOptions_Clear_Flag 0x02
-#define EventOptions_Clear 0x00
-#define EventOptions_NoClear EventOptions_Clear_Flag
+#define EventOptions_Clear_Flag 0x01
+#define EventOptions_NoClear 0x00
+#define EventOptions_Clear EventOptions_Clear_Flag
 
-class EventGroup
-{
-  public:
-    EventGroup(const char *name);
-    ~EventGroup();
-    Result set(uint32_t flags);
-    Result reset(uint32_t flags);
-    Result wait(uint32_t flags, uint32_t &actualFlags, EventOptions options, uint32_t timeout);
+	class EventGroup
+	{
+	 public:
+		EventGroup(const char* name);
+		~EventGroup();
+		Result set(uint32_t flags);
+		Result reset(uint32_t flags);
+		Result wait(uint32_t flags, uint32_t& actualFlags, EventOptions options, uint32_t timeout);
 
-  private:
-    EVENTGROUP_TYPEDEF _instance;
-};
+	 private:
+		EVENTGROUP_TYPEDEF _instance;
+	};
 } // namespace wibot::os
 
 #endif // ___OS_PORT_HPP__
