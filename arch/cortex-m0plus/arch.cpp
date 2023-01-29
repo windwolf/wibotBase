e588fcda5a39999abc668b52bc5ddb1624631ea3
zhoujian <zhoujian.ww@gmail.com>
2023-01-29 16:07:42 +0800

    增加   arch

--- arch/cortex-m0plus/arch.cpp
+++ arch/cortex-m0plus/arch.cpp
@@ -7,6 +7,16 @@ namespace wibot::arch
 {
     bool sync_compare_and_swap(volatile uint32_t* ptr, uint32_t old_value, uint32_t new_value)
     {
+//        uint32_t oldval, res;
+//        __asm__ __volatile__("@ sync_compare_and_swap\n"
+//                             "ldrex  %1, [%2]\n"
+//                             "mov    %0, #0\n"
+//                             "teq    %1, %3\n"
+//                             "strexeq %0, %4, [%2]\n"
+//            : "=&r" (res), "=&r" (oldval)
+//            : "r" (ptr), "Ir" (old_value), "r" (new_value)
+//            : "cc");
+//        return res;
         if (*ptr == old_value)
         {
             *ptr = new_value;
@@ -14,7 +24,7 @@ namespace wibot::arch
         }
         else
         {
-            return true;
+            return false;
         }
 
     };
