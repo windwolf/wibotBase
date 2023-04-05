93272d534587eff5fc9bf22005a22261fc51ce1a
zhoujian <zhoujian.ww@gmail.com>
2023-04-05 22:09:16 +0800

    uart   修复错误处理，增加日志，增加内部计数状态

--- peripheral/uart.hpp
+++ peripheral/uart.hpp
@@ -4,63 +4,57 @@
 #include "peripheral.hpp"
 #include "ringbuffer.hpp"
 
-namespace wibot::peripheral
-{
+namespace wibot::peripheral {
 #define UART_PER_DECL
 
-	union UARTConfig
-	{
-		struct
-		{
-			DataWidth dataWidth: 2;
-            bool ignore_parity_error: 1;
-            bool ignore_frame_error: 1;
-            bool ignore_overrun_error: 1;
-            bool ignore_noise_error: 1;
-			uint32_t : 26;
-		};
-		uint32_t value;
-	};
+union UARTConfig {
+    struct {
+        DataWidth dataWidth : 2;
+        bool ignore_parity_error : 1;
+        bool ignore_frame_error : 1;
+        bool ignore_overrun_error : 1;
+        bool ignore_noise_error : 1;
+        uint32_t : 26;
+    };
+    uint32_t value;
+};
 
-	class UART : public Initializable, public Configurable<UARTConfig>
-	{
-	 public:
-		UART(UART_CTOR_ARG);
-		Result _init() override;
-		void _deinit() override;
+class UART : public Initializable, public Configurable<UARTConfig> {
+   public:
+    UART(UART_CTOR_ARG, const char *name);
+    Result _init() override;
+    void _deinit() override;
 
-		Result read(void* data, uint32_t size, WaitHandler& waitHandler);
-		Result write(void* data, uint32_t size, WaitHandler& waitHandler);
-		Result start(RingBuffer &rxBuffer, WaitHandler& waitHandler);
-		Result stop();
+    Result read(void *data, uint32_t size, WaitHandler &waitHandler);
+    Result write(void *data, uint32_t size, WaitHandler &waitHandler);
+    Result start(RingBuffer &rxBuffer, WaitHandler &waitHandler);
+    Result stop();
 
-	 private:
-		UART_FIELD_DECL
-		union
-		{
-			struct
-			{
-				bool isTxDmaEnabled: 1;
-				bool isRxDmaEnabled: 1;
-			};
-			uint32_t value;
-		} _status;
+   public:
+    UART_FIELD_DECL
+    struct {
+        bool isTxDmaEnabled : 1;
+        bool isRxDmaEnabled : 1;
+    } _status;
+    uint32_t _errorCount;
+    uint32_t _rxCount;
+    uint32_t _txCount;
 
-		WaitHandler* _writeWaitHandler;
-		WaitHandler* _readWaitHandler;
-        RingBuffer* cirRxBuffer_;
+    WaitHandler *_writeWaitHandler;
+    WaitHandler *_readWaitHandler;
+    RingBuffer *cirRxBuffer_;
+    const char *name_;
 
-	 protected:
-		static void _on_read_complete_callback(UART_CALLBACK_ARG);
-		static void _on_write_complete_callback(UART_CALLBACK_ARG);
-		static void _on_circular_data_received_callback(UART_CALLBACK_ARG, uint16_t pos);
-		static void _on_error_callback(UART_CALLBACK_ARG);
-	};
+   protected:
+    static void _on_read_complete_callback(UART_CALLBACK_ARG);
+    static void _on_write_complete_callback(UART_CALLBACK_ARG);
+    static void _on_circular_data_received_callback(UART_CALLBACK_ARG, uint16_t pos);
+    static void _on_error_callback(UART_CALLBACK_ARG);
+};
 
-} // namespace wibot::peripheral
+}  // namespace wibot::peripheral
 
-extern "C"
-{
-void uart_send_byte(const char* data, uint16_t len);
+extern "C" {
+void uart_send_byte(const char *data, uint16_t len);
 }
-#endif // __WWDEVICE_PERIPHERAL_UART_HPP__
+#endif  // __WWDEVICE_PERIPHERAL_UART_HPP__
